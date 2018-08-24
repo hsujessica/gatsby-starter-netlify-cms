9fba34de7ecf67c9b7e414d3b4c40d18200b9538
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-08-24 16:37:33 -0400

    add twitter to contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const ContactPage = () => (
-  <div className='contact-links'>
-    <a href='mailto:jessica.y.hsu+website@gmail.com'>Email</a>
-    <a href='https://github.com/hsujessica'>Github</a>
-    <a href='https://www.linkedin.com/in/jessica-hsu/'>LinkedIn</a>
-    <a href='https://twitter.com/chaussure'>Twitter</a>
-    <a href='https://www.instagram.com/jessicahsu_/'>Instagram</a>
+  <div className="contact-links">
+    <a href="mailto:jessica.y.hsu+website@gmail.com">Email</a>
+    <a href="https://github.com/hsujessica">Github</a>
+    <a href="https://www.linkedin.com/in/jessica-hsu/">LinkedIn</a>
+    <a href="https://twitter.com/jessicahsu_">Twitter</a>
+    <a href="https://www.instagram.com/jessicahsu_/">Instagram</a>
   </div>
 )
 
