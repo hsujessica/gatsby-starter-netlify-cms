57bc098bb55c387169699e2f2d07a46be3be0009
Jessica H <jessica@MacBook-Air.local>
2021-09-01 16:53:02 -0700

    update about and css

--- src/components/About.js
+++ src/components/About.js
@@ -5,16 +5,16 @@ const About = () => {
   return (
     <div className="about">
       <div className="about-section">
-        <p>Hi there! I'm Jessica.</p>
+        <p>Hi, I'm Jessica.</p>
         <p>Coder, crafter, cruciverbalist.</p>
-        <p>Currently at Dropbox, SF.</p>
+        <p>Currently at Square, SF. Previously Dropbox.</p>
       </div>
       <div className="about-section">
         <Contact />
       </div>
       <div className="about-section">
-        <p>Currently reading: The Body Keeps the Score - Bessel van der Kolk</p>
-        <p>Currently listening to: Dawn, the Front - Talos</p>
+        <p>Currently reading: <i>On Looking: A Walker's Guide to the Art of Observation - Alexandra Horowitz</i></p>
+        <p>Currently listening to: <i>Le Point Zéro - Freedom Fry</i></p>
       </div>
     </div>
   )
